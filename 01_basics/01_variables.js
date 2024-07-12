7a2e6fd66ce2bb0c49dbd3478f3333b28ba38113
Tuhin Ghosh <116188971+Tuhinghosh4@users.noreply.github.com>
2024-07-12 10:05:00 +0000

    some changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "tuhin56@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "2122123"
+accountCity = "Bengaluru" 
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS Code as newer version
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3);// code readability should be high
+
+console.log("Tuhin")
+
+let name = "Tuhin"
+let age = 19
+let isLoggedIn = false
+let state = null;
+
+// number => 2 to the power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof"tuhin");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined)
